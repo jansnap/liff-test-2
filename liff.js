a6f88467d2845225327a6720e06fb9d74891d795
Katsuhiko Okano <okano@jansnap.com>
2025-07-11 11:35:13 +0900

    arrange LIFF ID

--- liff.js
+++ liff.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     // liffId: LIFF URL "https://liff.line.me/xxx"のxxxに該当する箇所
     // LINE DevelopersのLIFF画面より確認可能
-    var liffId = "1656008688-8GV3XAYb";
+    var liffId = "2007722359-Gb2v5B5r";
     initializeLiff(liffId);
 })
 
